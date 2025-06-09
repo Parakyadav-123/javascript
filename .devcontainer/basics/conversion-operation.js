6fad63961e6d443f1d683ec8ae6ef2a19ce7addd
Parakyadav-123 <parakydv@gmail.com>
2025-06-09 22:31:10 +0000

    covertion datattype

---
+++ .devcontainer/basics/conversion-operation.js
@@ -0,0 +1,27 @@
+let score="paral"
+console.log(typeof score);
+let valueinNumber = Number(score)
+console.log(valueinNumber);
+console.log(typeof valueinNumber);
+
+//"33" => 33
+//33abc => nan
+//string , undefined => nan
+//null => 0
+
+let iswhat= ""
+let see = Boolean(iswhat)
+console.log(see);
+
+//1=true 0=false
+// ""= false
+// "abc"= true
+
+let num=23
+let stringnum = String(num)
+console.log(typeof stringnum);
+console.log(stringnum);
+
+
+
+
