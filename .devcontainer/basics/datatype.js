e0db60838aecf259e59bca45e5144077e0ec902f
Parakyadav-123 <parakydv@gmail.com>
2025-06-09 21:09:38 +0000

    datatype commit

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,19 @@
+"use strict" //trat all js code as newer version
+// alert()  //we are using node js not browser
+let name = "parak"
+let age =18
+let islogin = false
+let store =  null
+
+//number => 2 to power 53
+// strings => "" , ''
+// boolean => t/f
+//null => standalone value 
+// undefined => not assigned value
+// symbol=> unique
+
+//above are premitive data type
+
+console.log(typeof age);
+
+console.log(typeof null) // null is a object 
---
+++ .devcontainer/basics/var.js
@@ -0,0 +1,13 @@
+const accId = 14
+let accEmai = "yadav@gmail.com"
+var accPass = "jaipur"
+accCity="meeruut"
+let accsate;
+
+// accId = 67
+accEmai = "ac@gmail"
+accPass= "pa"
+accCity="delhi" 
+
+console.log(accId);
+console.table([accEmai,accPass, accCity, accId , accsate]);
