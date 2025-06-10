485f83f6b4518c1d657f6b6b514fe6198203827c
Parakyadav-123 <parakydv@gmail.com>
2025-06-10 10:40:03 +0000

    operational changes

--- .devcontainer/basics/conversion-operation.js
+++ .devcontainer/basics/conversion-operation.js
@@ -22,6 +22,67 @@ let stringnum = String(num)
 console.log(typeof stringnum);
 console.log(stringnum);
 
+let value =3
+let negativeNum= -value
+console.log(  negativeNum);
+
+let str1 =  "parak"
+let str2 = "yadav"
+let str3 = str1+str2
+console.log(str3);
+
+//complex sitiuation
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2+2);
+console.log(1+2+"3");
+console.log(1);
+console.log(+true); //not a good practice 
+console.log(+""); //not a good practice
+
+let gameCounter= 100
+gameCounter++
+console.log(gameCounter);
+
+//learn prefix and postfix
+
+//comparision of datatype
+
+//problem occur comparing diff data type
+
+console.log("2" > 1);
+console.log("02"> 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0)
+
+//avoid
+// ===
+console.log("2"===2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
