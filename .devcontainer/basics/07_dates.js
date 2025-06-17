bd91caf23e25629c345208e31f66e5e71849c8e8
Parakyadav-123 <parakydv@gmail.com>
2025-06-17 09:25:33 +0000

    date and time

---
+++ .devcontainer/basics/07_dates.js
@@ -0,0 +1,44 @@
+//date 
+// let mydate = new Date();
+// console.log(mydate);
+// console.log(mydate.toString);
+// console.log(mydate.toDateString);
+// console.log(mydate.toLocaleString);
+
+// const myDate = new Date();
+// console.log(myDate.toString());         // Outputs a full human-readable date
+// console.log(myDate.toDateString());     // Outputs just the date portion
+// console.log(myDate.toLocaleString());   // Outputs the date/time in local format
+// console.log(typeof myDate);
+// console.log(myDate.toDateString());
+// let ymcreateddate = new Date(2025 ,1 ,23)
+// console.log(ymcreateddate.toDateString());
+// console.log(ymcreateddate.toLocaleString());
+// console.log(ymcreateddate.toLocaleDateString
+//     ());
+    
+
+// let createdate = new Date ("04-08-2002")
+// console.log(createdate.toDateString());
+// console.log(createdate.toString());
+// console.log(createdate.toString());
+// console.log(createdate.toLocaleString());
+
+let mytimestampm = Date.now()
+// console.log(mytimestampm);
+
+// console.log(createdate.getTime());
+console.log(Date.now()); //milisec
+console.log(Math.floor(Date.now()/1000)); //sec //interviewww
+//interview
+
+let newdate =new Date()
+
+console.log(newdate);
+console.log(newdate.getMonth()); // start index from 0 
+console.log(newdate.getDay()); // output in no. form
+
+
+
+
+
